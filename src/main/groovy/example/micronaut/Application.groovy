12c2462c19937d1c9becf00b1ebd1894cc75f2a5
Adrien Bonnin <adrien.bonnin@cbp-group.com>
2018-10-09 11:35:16 +0200

    micronaut issue for OpenAPI generation with Groovy project

---
+++ src/main/groovy/example/micronaut/Application.groovy
@@ -0,0 +1,24 @@
+package example.micronaut
+
+import io.micronaut.runtime.Micronaut
+import groovy.transform.CompileStatic
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Hello World",
+                version = "0.0",
+                description = "My API",
+                license = @License(name = "Apache 2.0", url = "http://foo.bar"),
+                contact = @Contact(url = "http://gigantic-server.com", name = "Fred", email = "Fred@gigagantic-server.com")
+        )
+)
+@CompileStatic
+class Application {
+    static void main(String[] args) {
+        Micronaut.run(Application)
+    }
+}
